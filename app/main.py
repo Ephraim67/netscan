b5681284fee0eda9fd8199fa757c61305cbbe8aa
Ephraim67 <jonathaneuphrem@gmail.com>
2025-07-02 15:24:09 +0100

    Commented out the static dir for now.

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 )
 
 # Mount static and template folders
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # Logging config
